3b5109d79f6e9f9e62195fa2f4d3217dd1bbb7f3
Kelvin Fann <kelvinfann@gmail.com>
2015-06-09 15:03:09 -0700

    added one test

--- spec/filters/intervalmetric_spec.rb
+++ spec/filters/intervalmetric_spec.rb
@@ -12,5 +12,18 @@ describe LogStash::Filters::IntervalMetric do
         insist { events }.nil? 
       end # it "should not do anything"
     end # context "no events were receieved"
+    context "when one event was received" do
+      subject {
+        config = {"counter" => ["one"]}
+        filter = Logstash::Filters::IntervalMetric.new config
+        filter.register
+        filter.filter LogStash::Event.new({"response" => 200})
+        filter.flush
+      } 
+      it "should output one" do
+        insist { subject.length } == 1
+        insist { subject.first["one.count"] } == 1 
+      end
+    end
   end # context "basic counter"
 end # describe Logstash::Filters:IntervalMetric
