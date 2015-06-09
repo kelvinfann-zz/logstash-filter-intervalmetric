0d1301cb6710d943fb5f643d6a646e3024e5d37d
Kelvin Fann <kelvinfann@gmail.com>
2015-06-09 15:49:34 -0700

    added rough counter 6 test

--- spec/filters/intervalmetric_spec.rb
+++ spec/filters/intervalmetric_spec.rb
@@ -14,16 +14,30 @@ describe LogStash::Filters::IntervalMetric do
     end # context "no events were receieved"
     context "when one event was received" do
       subject {
-        config = {"counter" => ["one"], "count_interval" => -10}
+        config = {"counter" => ["one"], "count_interval" => -5}
         filter = LogStash::Filters::IntervalMetric.new config
         filter.register
         filter.filter LogStash::Event.new({"response" => 200})
         filter.flush
       } 
-      it "should output one" do
+      it "should have a counter of 1" do
         insist { subject.length } == 1
         insist { subject.first["one.count"] } == 1 
-      end
-    end
+      end # it "should output one"
+      it "should have a counter of 6" do
+        config = {"counter" => ["six"], "counter_interval => 0"}
+        filter = LogStash::Filters::IntervalMetric.new config
+        filter.register
+        for i in 0..6
+          filter.filter LogStash::Event.new({"response" => i})
+        end # for i in 0..6
+        event = filter.flush
+        insist { event.length } == 1
+        insist { event.first["six.count"] } == 6
+      end # it "should output 6"
+    end # context "when one event was received"
+    context "when one event was received" do 
+    end # context "when multiple events were received"
+
   end # context "basic counter"
 end # describe Logstash::Filters:IntervalMetric
