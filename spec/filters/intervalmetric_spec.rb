cd6373526515c83997c1791c204d2b863a74516c
cloudera <cloudera@quickstart.cloudera>
2015-06-10 11:06:20 -0700

    removed infinate loop errors

--- spec/filters/intervalmetric_spec.rb
+++ spec/filters/intervalmetric_spec.rb
@@ -14,10 +14,12 @@ describe LogStash::Filters::IntervalMetric do
     end # context "no events were receieved"
     context "when one event was received" do
       subject {
-        config = {"counter" => ["one"], "count_interval" => -5}
+        config = {"counter" => ["one"], "count_interval" => 5}
         filter = LogStash::Filters::IntervalMetric.new config
         filter.register
         filter.filter LogStash::Event.new({"response" => 200})
+        filter.flush
+        filter.flush
         filter.flush
       } 
       it "should have a counter of 1" do
@@ -25,13 +27,14 @@ describe LogStash::Filters::IntervalMetric do
         insist { subject.first["one.count"] } == 1 
       end # it "should output one"
       it "random counter test" do
-        config = {"counter" => ["six"], "count_interval" => 0}
+        config = {"counter" => ["six"], "count_interval" => 5}
         filter = LogStash::Filters::IntervalMetric.new config
         filter.register
         r = rand(2..100)
         for i in 1..r
           filter.filter LogStash::Event.new({"response" => i})
         end # for i in 0..r
+        event = filter.flush
         event = filter.flush
         insist { event.length } == 1
         insist { event.first["six.count"] } == r
@@ -41,18 +44,19 @@ describe LogStash::Filters::IntervalMetric do
     end # context "when multiple events were received"
     context "when we have different counters" do
       subject {
-        config = {"counter" => ["%{response}"], "count_interval" => -5}
+        config = {"counter" => ["%{response}"], "count_interval" => 5}
         filter = LogStash::Filters::IntervalMetric.new config
         filter.register
         filter.filter LogStash::Event.new({"response" => 200})
         filter.filter LogStash::Event.new({"response" => 400})
         filter.filter LogStash::Event.new({"response" => 200})
         filter.flush
+        filter.flush
       } 
       it "should have a counter of 1" do
         insist { subject.length } == 1
         insist { subject.first["200.count"] } == 2
-        inisit { subject.first["400.count"] } == 1 
+        insist { subject.first["400.count"] } == 1 
       end # it "should have multiple counters"          
     end # context "when we have different counters"
   end # context "basic counter"
